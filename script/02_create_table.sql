c74f9ccd59e972cce3bad92192d49aa534454bb7
faizashareef2000-jpg <faizashareef2000@gmail.com>
2026-04-13 00:09:10 +0530

    Create 02_create_table.sql

---
+++ script/02_create_table.sql
@@ -0,0 +1,81 @@
+-- Create Bronze Layer Tables
+-- ===========================================================================
+  --CRM Tables
+-- ===========================================================================
+
+--Customer Information
+IF OBJECT_ID('bronze.crm_cust_info') IS NOT NULL
+DROP TABLE bronze.crm_cust_info;
+
+CREATE TABLE bronze.crm_cust_info(
+ cst_id INT,
+ cst_key NVARCHAR(50),
+ cst_firstname NVARCHAR(50),
+ cst_lastname NVARCHAR(50),
+ cst_material_status NVARCHAR(50),
+ cst_gndr NVARCHAR(50),
+ cst_create_date DATE
+);
+
+-- Product Information
+IF OBJECT_ID('bronze.crm_prd_info') IS NOT NULL
+DROP TABLE bronze.crm_prd_info;
+
+CREATE TABLE bronze.crm_prd_info(
+ prd_id INT,
+ prd_key NVARCHAR(50),
+ prd_nm NVARCHAR(50),
+ prd_cost INT,
+ prd_line NVARCHAR(50),
+ prd_start_dt DATETIME,
+ prd_end_dt DATETIME
+);
+
+--Sales Details
+IF OBJECT_ID('bronze.crm_sales_details_info') IS NOT NULL
+DROP TABLE bronze.crm_sales_details_info;
+
+ CREATE TABLE bronze.crm_sales_details_info(
+  sls_ord_num NVARCHAR(50),
+  sls_prd_key NVARCHAR(50),
+  sls_cust_id INT,
+  sls_order_dt INT,
+  sls_ship_dt INT,
+  sls_due_dt INT,
+  sls_sales INT,
+  sls_quantity INT,
+  sls_price INT
+);
+-- =======================================================================
+--ERP Tables
+--========================================================================
+
+-- Location Data
+IF OBJECT_ID('bronze.erp_loc_a101') IS NOT NULL
+DROP TABLE bronze.erp_loc_a101;
+
+CREATE TABLE bronze.erp_loc_a101(
+ cid NVARCHAR(50),
+ cntry NVARCHAR(50)
+);
+
+-- Customer Data
+IF OBJECT_ID('bronze.erp_cust_az12') IS NOT NULL
+DROP TABLE bronze.erp_cust_az12;
+
+CREATE TABLE bronze.erp_cust_az12(
+cid NVARCHAR(100),
+bdate DATE,
+gen NVARCHAR(100)
+);
+
+-- Product Category
+IF OBJECT_ID('bronze.erp_px_cat_g1v2') IS NOT NULL
+DROP TABLE bronze.erp_px_cat_g1v2;
+
+CREATE TABLE bronze.erp_px_cat_g1v2(
+id NVARCHAR(50),
+cat NVARCHAR(50),
+subcat NVARCHAR(50),
+mantainence NVARCHAR(50)
+);
